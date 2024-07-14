3257e9d6242955980a34f3831bfa95779909bdc3
devcho <myongsg@gmail.com>
2024-07-14 20:11:29 +0900

    first commit

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, createContext, useState } from "react";
+
+const ThemeContext = createContext({
+    theme: 'light',
+    toggleMode: () => {},
+});
+
+interface ThemeProps {
+    children: ReactNode;
+}
+
+export const ThemeContextProvider = ({ children }: ThemeProps) => {
+    const [theme, SetTheme] = useState(window.localStorage.getItem("theme") || "light");
+    const toggleMode = () => {
+        SetTheme((prev) => (prev === "light" ? "dark" : "light"));
+        window.localStorage.setItem("theme", theme === "light" ? "dark" : "light"); 
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleMode}}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export default ThemeContext;
